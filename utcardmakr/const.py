7443f5631ce96e81d00c799505ff37dc42abecf0
Anas Ouh <anasouhtrcg@gmail.com>
2023-10-19 15:53:38 +0200

    editing const.py

--- utcardmakr/const.py
+++ utcardmakr/const.py
@@ -3,27 +3,27 @@ import pkgutil
 
 CARD_BG = {
     "common": {
-        "bronze": pkgutil.get_data(__name__, "assets/cards/common/bronze.png").replace("\\", "/"),
-        "silver": pkgutil.get_data(__name__, "assets/cards/common/silver.png").replace("\\", "/"),
-        "gold": pkgutil.get_data(__name__, "assets/cards/common/gold.png").replace("\\", "/"),
+        "bronze": pkgutil.get_data(__name__, "assets/cards/common/bronze.png"),
+        "silver": pkgutil.get_data(__name__, "assets/cards/common/silver.png"),
+        "gold": pkgutil.get_data(__name__, "assets/cards/common/gold.png"),
     },
     "rare": {
-        "bronze": pkgutil.get_data(__name__, "assets/cards/rare/bronze.png").replace("\\", "/"),
-        "silver": pkgutil.get_data(__name__, "assets/cards/rare/silver.png").replace("\\", "/"),
-        "gold": pkgutil.get_data(__name__, "assets/cards/rare/gold.png").replace("\\", "/"),
+        "bronze": pkgutil.get_data(__name__, "assets/cards/rare/bronze.png"),
+        "silver": pkgutil.get_data(__name__, "assets/cards/rare/silver.png"),
+        "gold": pkgutil.get_data(__name__, "assets/cards/rare/gold.png"),
     },
 }
 
 CARD_BG_SMALL = {
     "common": {
-        "bronze": pkgutil.get_data(__name__, "assets/cards/small/common/bronze.png").replace("\\", "/"),
-        "silver": pkgutil.get_data(__name__, "assets/cards/small/common/silver.png").replace("\\", "/"),
-        "gold": pkgutil.get_data(__name__, "assets/cards/small/common/gold.png").replace("\\", "/"),
+        "bronze": pkgutil.get_data(__name__, "assets/cards/small/common/bronze.png"),
+        "silver": pkgutil.get_data(__name__, "assets/cards/small/common/silver.png"),
+        "gold": pkgutil.get_data(__name__, "assets/cards/small/common/gold.png"),
     },
     "rare": {
-        "bronze": pkgutil.get_data(__name__, "assets/cards/small/rare/bronze.png").replace("\\", "/"),
-        "silver": pkgutil.get_data(__name__, "assets/cards/small/rare/silver.png").replace("\\", "/"),
-        "gold": pkgutil.get_data(__name__, "assets/cards/small/rare/gold.png").replace("\\", "/"),
+        "bronze": pkgutil.get_data(__name__, "assets/cards/small/rare/bronze.png"),
+        "silver": pkgutil.get_data(__name__, "assets/cards/small/rare/silver.png"),
+        "gold": pkgutil.get_data(__name__, "assets/cards/small/rare/gold.png"),
     },
 }
 
